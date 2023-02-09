3a555fae6665688c8850b49e48061b6342d8e49d
Hasib-Ahmed-681 <119836032+Hasib-Ahmed-681@users.noreply.github.com>
2023-02-09 20:06:01 +0600

    Add files via upload

---
+++ Graph/Main.java
@@ -0,0 +1,17 @@
+package Graph;
+
+public class Main {
+    public static void main(String [] args){
+        var graph = new Graph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+
+        graph.addEdge("A","B");
+        graph.addEdge("A","C");
+
+        graph.removeEdge("A","C");
+        // graph.removeNode("B");
+        graph.print();
+    }
+}
